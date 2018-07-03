b7fec6ba0d72084e3745f421ed7ba86f13d9e45a
Atticus Pomerantz <rapomerantz@gmail.com>
2018-07-03 14:35:31 -0500

    Daily weather API working:

--- src/OpenWeatherTest.jsx
+++ src/OpenWeatherTest.jsx
@@ -5,47 +5,67 @@ import './main.css'
 
 import axios from 'axios'; 
 
-
-
 class OpenWeatherTest extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            zipCode: 55305,
-            apiResponse: TestData,
+            zipCode: 55418,
+            apiResponse: TestData
         }
     }
 
-    axiosTestCall = () => {
-        console.log('in AxiosTestCall');
-        axios.get('https://swapi.co/api/people/?search=skywalker')
+    getWeather = () => {
+        let zipCode = this.state.zipCode
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&APPID=59d1b54e2c5f610efe6e684af2ba76c6`)
         .then((response) => {
-            console.log('response', response.data);
+            console.log('Successful GET',response.data);
+            console.log(response.data.weather[0].icon);
+            this.setState({
+                apiResponse: response.data
+            })            
         })
         .catch((error) => {
             console.log('error', error);
         })
     }
 
+    // getWeatherIcon = (iconCode) => {
+    //     axios.get(`http://openweathermap.org/img/w/${iconCode}.png`)
+    //     .then((response) => {
+    //         console.log('successful GET of weather Icon');
+    //         this.setState({
+    //             weatherIcon: response.data
+    //         })
+    //     })
+    //     .catch((error) => {
+    //         console.log('error in Icon');
+    //     })
+    // }
+
+
     handleChangeZip = (event) => {
-        console.log(event.target.value);
-        
+        this.setState({
+            zipCode: event.target.value        
+        })
     }
 
 
   render() {
     const {apiResponse} = this.state
-
-      let sunrise = (new Date (apiResponse.sys.sunrise * 1000)).toLocaleString();
-      let sunset = (new Date (apiResponse.sys.sunset * 1000)).toLocaleString();
+    let sunrise = (new Date (apiResponse.sys.sunrise * 1000)).toLocaleString();
+    let sunset = (new Date (apiResponse.sys.sunset * 1000)).toLocaleString();
 
     return (
       <div className="OpenWeatherTest">
         <h1>What's The Weather Right Now?</h1>
         <pre id="jsonPrint">{JSON.stringify(this.state, null, 2)}</pre>
         <div>
-            <button onClick={this.axiosTestCall}>Make the Call</button>
+            <input type="number" 
+                    name="zipCode" 
+                    onChange = {this.handleChangeZip}
+                    value={this.state.zipCode}/>
+            <button onClick={this.getWeather}>Make the Call</button>
 
             <h2>City: {apiResponse.name}</h2>
             <h3>Current Temp: {apiResponse.main.temp} {String.fromCharCode(8457)}</h3>
